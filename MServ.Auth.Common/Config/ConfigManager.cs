7156b398b7280af771f94ceb60db7b42974fc719
Bogdan Hardau <alboghardau@gmail.com>
2022-02-17 01:01:51 +0200

    updates

---
+++ MServ.Auth.Common/Config/ConfigManager.cs
@@ -0,0 +1,48 @@
+﻿using MServ.Auth.Common.Interfaces;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MServ.Auth.Common.Config
+{
+    public class ConfigManager : IConfigManager     
+    {
+        JObject config;
+        public ConfigManager()
+        {
+            string path = Path.Combine(AppContext.BaseDirectory, "Config.json");
+            config = JObject.Parse(path);
+        }
+
+        public string GetString(string jsonPath)
+        {
+            return ReadValue(jsonPath);
+        }
+
+        public bool GetBool(string jsonPath)
+        {
+            var value = ReadValue(jsonPath);
+            if(value == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string ReadValue(string jsonPath)
+        {
+            var value = config.SelectToken(jsonPath);
+            if(value != null)
+            {
+                return value.ToString();
+            }
+            return String.Empty;
+        }
+    }
+}
---
+++ MServ.Auth.Common/Interfaces/IConfigManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MServ.Auth.Common.Interfaces
+{
+    public  interface IConfigManager
+    {
+        public string GetString(string jsonPath);
+        public bool GetBool(string jsonPath);
+    }
+}
